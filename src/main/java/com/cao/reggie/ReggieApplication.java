ae9a829d0b4b26ffec92b4e83b3c5496604090f8
schwarzenegger <1225060015@qq.com>
2022-04-18 20:21:34 +0800

    配置文件、前端页面初始搭建

---
+++ src/main/java/com/cao/reggie/ReggieApplication.java
@@ -0,0 +1,14 @@
+package com.cao.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功...");
+    }
+}
