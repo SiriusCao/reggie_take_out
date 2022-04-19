0c5c79bd86777e2f55fd26d26b167e477fcb518d
schwarzenegger <1225060015@qq.com>
2022-04-19 10:03:13 +0800

    Employee的Service和Mapper

---
+++ src/main/java/com/cao/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.cao.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.cao.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
