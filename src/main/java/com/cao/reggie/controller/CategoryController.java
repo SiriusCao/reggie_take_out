d176437a7afcc2eb8fed4581a6e12ef35121a661
schwarzenegger <1225060015@qq.com>
2022-04-21 11:48:17 +0800

    分页查询

--- src/main/java/com/cao/reggie/controller/CategoryController.java
+++ src/main/java/com/cao/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.cao.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cao.reggie.common.R;
 import com.cao.reggie.entity.Category;
 import com.cao.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -23,13 +22,34 @@ public class CategoryController {
 
     /**
      * 新增分类
+     *
      * @param category 分类实体类
      * @return 状态信息
      */
     @PostMapping
-    public R<String> save(@RequestBody Category category){
-        log.info("新增{}",category.toString());
+    public R<String> save(@RequestBody Category category) {
+        log.info("新增{}", category.toString());
         categoryService.save(category);
         return R.success("新增成功");
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param page     页码
+     * @param pageSize 每页显示几个数据
+     * @return 分页信息
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        //分页构造器
+        Page<Category> pageInfo = new Page(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        //查询
+        categoryService.page(pageInfo, queryWrapper);
+        //返回分页数据
+        return R.success(pageInfo);
     }
 }
