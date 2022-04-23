560d72e0e7719515f883e6ce8ea69beeba9a60ef
schwarzenegger <1225060015@qq.com>
2022-04-23 17:48:46 +0800

    短信验证码登录功能

--- src/main/java/com/cao/reggie/controller/UserController.java
+++ src/main/java/com/cao/reggie/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.cao.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.cao.reggie.common.CustomException;
 import com.cao.reggie.common.R;
 import com.cao.reggie.entity.User;
 import com.cao.reggie.service.UserService;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -26,17 +30,68 @@ public class UserController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    /**
+     * 发送短信验证码
+     *
+     * @param user
+     * @return
+     */
     @RequestMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user){
+    public R<String> sendMsg(@RequestBody User user) {
+        //获取手机号
         String phone = user.getPhone();
         log.info("手机号码{}发来验证码请求", phone);
-        if (StringUtils.isNotEmpty(phone)){
+        if (StringUtils.isNotEmpty(phone)) {
+            //生成刘伟验证码
             String code = ValidateCodeUtils.generateValidateCode(6).toString();
+
+            // TODO: 2022/4/23 使用短信服务 发送短信验证码给客户，没钱买服务器就不做了
+
+            //将验证码存入redis，有效期为300秒
             ValueOperations ops = redisTemplate.opsForValue();
-            ops.set(phone,code,300, TimeUnit.SECONDS);
-            log.info("验证码为{}",code);
+            ops.set(phone, code, 300, TimeUnit.SECONDS);
+            log.info("验证码为{}", code);
             return R.success("发送成功");
         }
         return R.error("发送失败");
+    }
+
+    /**
+     * 验证短信验证码完成登录操作
+     *
+     * @param phoneAndCode 存储手机号码和对应的短信验证码
+     * @return
+     */
+    @RequestMapping("/login")
+    public R<User> login(@RequestBody Map<String, String> phoneAndCode, HttpServletRequest request) {
+        //获取传回的手机号和验证码
+        String phone = phoneAndCode.get("phone");
+        String code = phoneAndCode.get("code");
+
+        //获取存储在redis当中的短信验证码，进行比对
+        ValueOperations ops = redisTemplate.opsForValue();
+        String codeInRedis = (String) ops.get(phone);
+        //如果为空则说明验证码已经失效或者手机号码错误
+        if (codeInRedis == null) {
+            throw new CustomException("验证码已经失效或者手机号码错误");
+        }
+        //比对是否成功
+        if (code.equals(codeInRedis)) {
+            //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册后自动登录
+            LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(wrapper);
+            if (user == null) {
+                //自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            //登录
+            request.getSession().setAttribute("user",user.getId());
+            return R.success(user);
+        }
+        return R.error("验证码错误");
     }
 }
