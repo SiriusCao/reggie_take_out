4a59acb61058c040ce89d5a2e16e2154e31895b9
schwarzenegger <1225060015@qq.com>
2022-04-24 15:17:59 +0800

    地址新增

--- src/main/java/com/cao/reggie/controller/AddressBookController.java
+++ src/main/java/com/cao/reggie/controller/AddressBookController.java
@@ -2,6 +2,7 @@ package com.cao.reggie.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.cao.reggie.common.BaseContext;
 import com.cao.reggie.common.R;
 import com.cao.reggie.entity.AddressBook;
 import com.cao.reggie.entity.User;
@@ -9,10 +10,7 @@ import com.cao.reggie.service.AddressBookService;
 import com.cao.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +38,14 @@ public class AddressBookController {
         List<AddressBook> addressBookList = addressBookService.list(wrapper);
 
         return R.success(addressBookList);
+    }
+
+    @PostMapping
+    public R<String> add(@RequestBody AddressBook addressBook){
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBookService.save(addressBook);
+        log.info("新增{}",addressBook.toString());
+        return R.success("新增成功");
     }
 
 }
