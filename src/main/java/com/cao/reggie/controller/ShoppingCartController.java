b37372279eed158d7cb4d04da9de5b428746323b
schwarzenegger <1225060015@qq.com>
2022-04-25 19:18:05 +0800

    购物车中的物品数量—1

--- src/main/java/com/cao/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/cao/reggie/controller/ShoppingCartController.java
@@ -70,4 +70,46 @@ public class ShoppingCartController {
         return R.error("删除失败");
     }
 
+
+    /**
+     * 购物车中的物品数量—1
+     *
+     * @param shoppingCart 当前物品的ID
+     * @return 返回被操作物品当前的实体类
+     */
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        //获取当前用户ID
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        //将用户ID加入条件过滤器
+        LambdaQueryWrapper<ShoppingCart> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(ShoppingCart::getUserId, currentId);
+
+        //判断是套餐还是蔡品,并分别赋予条件过滤器对应的ID
+        Long dishId = shoppingCart.getDishId();
+        if (dishId != null) {
+            wrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            wrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+
+        //获取对应的购物车记录
+        ShoppingCart one = shoppingCartService.getOne(wrapper);
+
+        if (one != null) {
+            Integer number = one.getNumber();
+            //如果购物车当中该条商品有不止一个，就将number减1然后更新
+            if (number > 1) {
+                one.setNumber(number - 1);
+                shoppingCartService.updateById(one);
+            } else if (number == 1) {
+                //如果购物车当中改价调记录为0，则清除该记录并将one实体类实例当中的number置为0
+                shoppingCartService.remove(wrapper);
+                one.setNumber(0);
+            }
+        }
+        return R.success(one);
+    }
 }
