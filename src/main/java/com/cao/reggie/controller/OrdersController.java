06fe78ef81499a262b8aa7ea84ff174c5df220fd
schwarzenegger <1225060015@qq.com>
2022-04-26 13:46:48 +0800

    查询订单

--- src/main/java/com/cao/reggie/controller/OrdersController.java
+++ src/main/java/com/cao/reggie/controller/OrdersController.java
@@ -1,14 +1,21 @@
 package com.cao.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cao.reggie.common.BaseContext;
 import com.cao.reggie.common.R;
+import com.cao.reggie.dto.OrdersDto;
+import com.cao.reggie.entity.OrderDetail;
 import com.cao.reggie.entity.Orders;
+import com.cao.reggie.service.OrderDetailService;
 import com.cao.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -17,10 +24,40 @@ public class OrdersController {
     @Autowired
     private OrdersService ordersService;
 
+    @Autowired
+    private OrderDetailService orderDetailService;
+
     @PostMapping("/submit")
     public R<String> submit(@RequestBody Orders orders){
         log.info("提交订单{}",orders.toString());
         ordersService.submit(orders);
         return R.success("提交成功");
+    }
+
+
+    @GetMapping("/userPage")
+    public R<Page<OrdersDto>> page(int page, int pageSize){
+        Page<Orders> pageInfo=new Page<>();
+        LambdaQueryWrapper<Orders> ordersWrapper=new LambdaQueryWrapper<>();
+        ordersWrapper.eq(Orders::getUserId, BaseContext.getCurrentId());
+        ordersService.page(pageInfo,ordersWrapper);
+
+        Page<OrdersDto> pageDtoInfo=new Page<>();
+        BeanUtils.copyProperties(pageInfo,pageDtoInfo,"records");
+
+        List<Orders> ordersList = pageInfo.getRecords();
+
+        List<OrdersDto> ordersDtoList = ordersList.stream().map(item -> {
+            OrdersDto ordersDto = new OrdersDto();
+            BeanUtils.copyProperties(item, ordersDto);
+            LambdaQueryWrapper<OrderDetail> orderDetailWrapper = new LambdaQueryWrapper<>();
+            orderDetailWrapper.eq(OrderDetail::getOrderId, item.getId());
+            List<OrderDetail> orderDetailList = orderDetailService.list(orderDetailWrapper);
+            ordersDto.setOrderDetails(orderDetailList);
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        pageDtoInfo.setRecords(ordersDtoList);
+        return R.success(pageDtoInfo);
     }
 }
--- src/main/java/com/cao/reggie/dto/OrdersDto.java
+++ src/main/java/com/cao/reggie/dto/OrdersDto.java
@@ -8,13 +8,13 @@ import java.util.List;
 @Data
 public class OrdersDto extends Orders {
 
-    private String userName;
-
-    private String phone;
-
-    private String address;
-
-    private String consignee;
+//    private String userName;
+//
+//    private String phone;
+//
+//    private String address;
+//
+//    private String consignee;
 
     private List<OrderDetail> orderDetails;
 	
