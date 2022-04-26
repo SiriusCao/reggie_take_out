ebe1fd0351b35508fbc6a5f6b9fb6ce4e16d6053
schwarzenegger <1225060015@qq.com>
2022-04-26 16:19:04 +0800

    再来一单

--- src/main/java/com/cao/reggie/controller/OrdersController.java
+++ src/main/java/com/cao/reggie/controller/OrdersController.java
@@ -7,13 +7,18 @@ import com.cao.reggie.common.R;
 import com.cao.reggie.dto.OrdersDto;
 import com.cao.reggie.entity.OrderDetail;
 import com.cao.reggie.entity.Orders;
+import com.cao.reggie.entity.ShoppingCart;
 import com.cao.reggie.service.OrderDetailService;
 import com.cao.reggie.service.OrdersService;
+import com.cao.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,24 +32,27 @@ public class OrdersController {
     @Autowired
     private OrderDetailService orderDetailService;
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
     @PostMapping("/submit")
-    public R<String> submit(@RequestBody Orders orders){
-        log.info("提交订单{}",orders.toString());
+    public R<String> submit(@RequestBody Orders orders) {
+        log.info("提交订单{}", orders.toString());
         ordersService.submit(orders);
         return R.success("提交成功");
     }
 
 
     @GetMapping("/userPage")
-    public R<Page<OrdersDto>> page(int page, int pageSize){
-        Page<Orders> pageInfo=new Page<>();
-        LambdaQueryWrapper<Orders> ordersWrapper=new LambdaQueryWrapper<>();
+    public R<Page<OrdersDto>> page(int page, int pageSize) {
+        Page<Orders> pageInfo = new Page<>();
+        LambdaQueryWrapper<Orders> ordersWrapper = new LambdaQueryWrapper<>();
         ordersWrapper.eq(Orders::getUserId, BaseContext.getCurrentId());
         ordersWrapper.orderByDesc(Orders::getOrderTime);
-        ordersService.page(pageInfo,ordersWrapper);
+        ordersService.page(pageInfo, ordersWrapper);
 
-        Page<OrdersDto> pageDtoInfo=new Page<>();
-        BeanUtils.copyProperties(pageInfo,pageDtoInfo,"records");
+        Page<OrdersDto> pageDtoInfo = new Page<>();
+        BeanUtils.copyProperties(pageInfo, pageDtoInfo, "records");
 
         List<Orders> ordersList = pageInfo.getRecords();
 
@@ -60,5 +68,51 @@ public class OrdersController {
 
         pageDtoInfo.setRecords(ordersDtoList);
         return R.success(pageDtoInfo);
+    }
+
+    /**
+     * 再次下单
+     *
+     * @param orders 包含需要重复下单的订单ID
+     * @return
+     */
+    @PostMapping("/again")
+    public R<String> againOrder(@RequestBody Orders orders){
+        //获取要重复下单的订单号
+        Long orderId = orders.getId();
+        //根据订单号获取订单内容
+        LambdaQueryWrapper<OrderDetail> orderDetailWrapper = new LambdaQueryWrapper<>();
+        orderDetailWrapper.eq(OrderDetail::getOrderId, orderId);
+        List<OrderDetail> orderDetailList = orderDetailService.list(orderDetailWrapper);
+        //根据订单内容查询到dishID或者setmealId并将其对应的其他信息再次加入购物车
+        List<ShoppingCart> shoppingCartList = orderDetailList.stream().map(item -> {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            //如果是普通菜品
+            if (item.getDishId() != null) {
+                shoppingCart.setName(item.getName());
+                shoppingCart.setImage(item.getImage());
+                shoppingCart.setUserId(BaseContext.getCurrentId());
+                shoppingCart.setDishId(item.getDishId());
+                shoppingCart.setDishFlavor(item.getDishFlavor());
+                shoppingCart.setNumber(item.getNumber());
+                shoppingCart.setAmount(item.getAmount());
+                shoppingCart.setCreateTime(LocalDateTime.now());
+            } else if (item.getSetmealId() != null) {
+                //如果是套餐
+                shoppingCart.setName(item.getName());
+                shoppingCart.setImage(item.getImage());
+                shoppingCart.setUserId(BaseContext.getCurrentId());
+                shoppingCart.setSetmealId(item.getDishId());
+                shoppingCart.setNumber(item.getNumber());
+                shoppingCart.setAmount(item.getAmount());
+                shoppingCart.setCreateTime(LocalDateTime.now());
+            }
+            return shoppingCart;
+        }).collect(Collectors.toList());
+        boolean b = shoppingCartService.saveBatch(shoppingCartList);
+        if (b) {
+            return R.success("");
+        }
+        return R.error("未知错误");
     }
 }
