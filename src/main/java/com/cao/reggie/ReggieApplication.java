ec74b0c4b1140bf3b9f78dfe6e580b0ccd279bfc
schwarzenegger <schwarzeneggercao@163.com>
2022-05-07 16:12:27 +0800

    配置SpringCache

--- src/main/java/com/cao/reggie/ReggieApplication.java
+++ src/main/java/com/cao/reggie/ReggieApplication.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableCaching //开启Spring Cache注解方式是缓存功能
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
